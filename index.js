6ed926c870a4b6b56e74cafc0e3fa6d700ccd532
kevinalbert7 <20ko@live.fr>
2021-12-16 16:15:49 +0100

    routes valides

--- index.js
+++ index.js
@@ -1,18 +1,21 @@
 const express = require("express")
 const app = express()
+const morgan = require("morgan")
+const cors = require ("cors")
 const port = 5000
 
-const associationsRoutes = require('./routes/associations')
+const associationsRoutes = require('./routes/assiociations')
 
 app.use(express.json())
+app.use(cors())
+app.use(morgan('tiny'))
 
 app.use('/associations', associationsRoutes)
 
 app.get('/', (req,res) => {
-    res.json()
+    console.log(associations)
+    res.json(associations)
 })
-
-
 
 app.listen(port, () =>{
     console.log(`server running at port ${port}`)
--- routes/assiociations.js
+++ routes/assiociations.js
@@ -1,13 +1,16 @@
 const express = require("express")
 const app = express()
-const { body, validationResult } = require('express-validator')
-
+// const { body, validationResult } = require('express-validator')
 
 let associations = require("../associations.json")
 
+app.get('/', (req, res) => {
+    res.json(associations)
+})
+
 app.get('/:slug', (req, res) => {
     const { slug } = req.params
-    const association = associations.find(association => association.name === slug)
+    const association = associations.find(association => association.slug === slug)
     res.json(association)
 })
 
